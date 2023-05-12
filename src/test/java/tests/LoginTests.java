72046ab95992bb650991db22d8cde085e169bbfe
Shilol Pesahov <pesahov777@gmail.com>
2023-05-12 20:53:37 +0300

    added login negative

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,60 @@
+package tests;
+
+import config.AppiumConfig;
+import models.Auth;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import screens.AuthenticationScreen;
+import screens.ContactListScreen;
+import screens.SplashScreen;
+
+public class LoginTests extends AppiumConfig {
+
+    @Test
+    public void loginSuccess(){
+        //boolean result = new SplashScreen(driver)
+                //.checkCurrentVersion("Version 1.0.0")
+        boolean result = new AuthenticationScreen(driver)
+                .fillEmail("shilol@gmail.com")
+                .fillPassword("Shilol12345$")
+                .submitLogin()
+                .isActivityTitleDisplayed("Contact list");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void loginSuccessModel(){
+        //boolean result = new SplashScreen(driver)
+                //.checkCurrentVersion("Version 1.0.0")
+        boolean result = new AuthenticationScreen(driver)
+                .fillLoginRegistrationForm(Auth.builder().email("shilol@gmail.com").password("Shilol12345$").build())
+                .submitLogin()
+                .isActivityTitleDisplayed("Contact list");
+        Assert.assertTrue(result);
+
+    }
+
+    @Test
+    public void loginSuccessModel2(){
+        Assert.assertTrue( new AuthenticationScreen(driver)
+                .fillLoginRegistrationForm(Auth.builder().email("noa@gmail.com").password("Nnoa12345$").build())
+                .submitLogin()
+                .isActivityTitleDisplayed("Contact list"));
+
+    }
+
+    @Test
+    public void loginWrongEmail(){
+        new AuthenticationScreen(driver)
+                .fillLoginRegistrationForm(Auth.builder().email("shilol@gmail.com").password("Shilol12345$").build())
+                .submitLoginNegative()
+                .isErrorMessageContainsText("Login or password incorrect");
+
+    }
+
+    @AfterMethod
+    public void postCondition(){
+        new ContactListScreen(driver).logout();
+    }
+}
